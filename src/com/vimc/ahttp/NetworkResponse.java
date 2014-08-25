4e200147d12afb75b18615ee7e87ee6c2aa2505f
charliu <lrh8080@gmail.com>
2014-08-25 14:08:17 +0800

    update

---
+++ src/com/vimc/ahttp/NetworkResponse.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.vimc.ahttp;
+
+import org.apache.http.HttpStatus;
+import org.apache.http.protocol.HTTP;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Data and headers returned from {@link Network#performRequest(Request)}.
+ */
+public class NetworkResponse {
+    /**
+     * Creates a new network response.
+     * @param statusCode the HTTP status code
+     * @param data Response body
+     * @param headers Headers returned with this response, or null for none
+     * @param notModified True if the server returned a 304 and the data was already in cache
+     */
+    public NetworkResponse(int statusCode, byte[] data, Map<String, String> headers,
+            boolean notModified) {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.headers = headers;
+        this.notModified = notModified;
+    }
+
+    public NetworkResponse(byte[] data) {
+        this(HttpStatus.SC_OK, data, Collections.<String, String>emptyMap(), false);
+    }
+
+    public NetworkResponse(byte[] data, Map<String, String> headers) {
+        this(HttpStatus.SC_OK, data, headers, false);
+    }
+
+    /** The HTTP status code. */
+    public final int statusCode;
+
+    /** Raw data from this response. */
+    public final byte[] data;
+
+    /** Response headers. */
+    public final Map<String, String> headers;
+
+    /** True if the server returned a 304 (Not Modified). */
+    public final boolean notModified;
+    
+    public String getCharset() {
+        String contentType = headers.get(HTTP.CONTENT_TYPE);
+        if (contentType != null) {
+            String[] params = contentType.split(";");
+            for (int i = 1; i < params.length; i++) {
+                String[] pair = params[i].trim().split("=");
+                if (pair.length == 2) {
+                    if (pair[0].equals("charset")) {
+                        return pair[1];
+                    }
+                }
+            }
+        }
+
+        return HTTP.DEFAULT_CONTENT_CHARSET;
+    }
+}
